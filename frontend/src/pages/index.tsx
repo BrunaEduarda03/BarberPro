2139a3af25180508ac1cc66ac119ed8e416745f8
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-03-08 15:58:49 -0300

    iniciando frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head> 
+        <title>BarberPro - Seu Sistema Completo</title>
+      </Head>
+      <div>
+        <h2>Home</h2>
+      </div>
+    </>
+  )
+}
