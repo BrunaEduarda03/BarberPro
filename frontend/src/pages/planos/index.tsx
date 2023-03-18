a080f292e3f848c922f61cea610d97841db81fda
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-03-17 23:52:35 -0300

    tela de planos

--- frontend/src/pages/planos/index.tsx
+++ frontend/src/pages/planos/index.tsx
@@ -7,12 +7,17 @@ import {
   useMediaQuery
 } from '@chakra-ui/react'
 import { Sidebar } from '../../components/siderbar';
+import { canSSRAuth } from '@/utils/canSSRAuth';
+import { setupAPIClient } from '@/services/api';
 
+interface PlanProps{
+  premium: boolean;
+}
 
-
-export default function Planos(){
+export default function Planos({premium}:PlanProps){
   const [isMobile] = useMediaQuery('(max-width: 500px)')
-
+  // console.log(premium);
+  
   return(
     <>
       <Head>
@@ -26,23 +31,28 @@ export default function Planos(){
         </Flex>
 
         <Flex ml={10} pb={8} maxW="780px" w="90%" direction="column" align="flex-start" justify="flex-start">
+        <Flex gap={4} w="100%" flexDirection={isMobile ? "column" : "row"}>
+          <Flex rounded={4} p={2} flex={1} bg="barber.400" flexDirection="column">
+            <Heading
+              textAlign="center"
+              fontSize="2xl"
+              mt={5} mb={5}
+              color="gray.100"
+            >
+              Plano Grátis
+            </Heading>
 
-          <Flex gap={4} w="100%" flexDirection={isMobile ? "column" : "row"}>
-              <Flex rounded={4} p={2} flex={1} bg="barber.400" flexDirection="column">
-                <Heading
-                textAlign="center"
-                fontSize="2xl"
-                mt={5} mb={5}
-                color="gray.100"
-                >
-                  Plano Grátis
-                </Heading>
+            <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>
+              Registrar cortes.
+            </Text>
+            <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>
+              Criar apenas 3 modelos de corte.
+            </Text>
+            <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>
+              Editar dados do perfil.
+            </Text>
 
-                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>Registrar cortes.</Text>
-                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>Criar apenas 3 modelos de corte.</Text>
-                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>Editar dados do perfil.</Text>
-              </Flex> 
-
+            </Flex> 
               <Flex rounded={4} p={2} flex={1} bg="barber.400" flexDirection="column">
                 <Heading
                 textAlign="center"
@@ -53,29 +63,85 @@ export default function Planos(){
                   Plano Premium
                 </Heading>
 
-                <Text color='#FFF' fontWeight="bold" ml={4} mb={4}>Registrar cortes ilimitados.</Text>
-                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>Criar modelos ilimitados.</Text>
-                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>Editar modelos de corte.</Text>
-                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>Editar dados do perfil.</Text>
-                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>Receber todas atualizações.</Text>
-              
+                <Text color='#FFF' fontWeight="bold" ml={4} mb={4}>
+                  Registrar cortes ilimitados.
+                </Text>
+                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>
+                  Criar modelos ilimitados.
+                </Text>
+                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>
+                  Editar modelos de corte.
+                </Text>
+                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>
+                  Editar dados do perfil.
+                </Text>
+                <Text color='#FFF'fontWeight="bold" ml={4} mb={4}>
+                  Receber todas atualizações.
+                </Text>
 
-                <Button
-                  bg="button.cta"
+                {premium ? (
+                <Flex direction='column'>
+                  <Button
+                    bg="#000"
+                    m={2}
+                    color="white"
+                    _hover={{bg:'#313131b4'}}
+                    onClick={() => {}}
+                    >
+                    VOCÊ JÁ É PREMIUM
+                  </Button>
+                  <Button
+                  bg="#fff"
                   m={2}
-                  color="white"
+                  color="#000"
+                  _hover={{bg:'#313131b4'}}
                   onClick={() => {}}
-                >
-                  VIRAR PREMIUM
+                  >
+                  ALTERAR ASSINATURA
                 </Button>
-
-              </Flex> 
-
-
           </Flex>
-
+         ):(
+          <Flex direction='column' >
+            <Text ml={4} color="#31fb6a" fontWeight='bold' fontSize='3xl'>R$ 9,99</Text>
+            <Button
+              bg="button.cta"
+              m={2}
+              color="white"
+              _hover={{bg:'#ffb13e'}}
+              onClick={() => {}}
+            >
+              VIRAR PREMIUM
+            </Button>
+          </Flex>
+         )}
+              </Flex>
+              
+          </Flex>  
         </Flex>
       </Sidebar>
     </>
   )
 }
+
+export const getServerSideProps = canSSRAuth(async(ctx)=>{
+  try{
+    const apiCLiente = setupAPIClient(ctx);
+    const response = await apiCLiente.get('/details');
+    
+    return {
+      props:{
+        premium:response.data?.subscriptions?.status === 'active' ? false:true,
+      }
+    }
+
+  }catch(err){
+    console.log(err);
+    return {
+      redirect:{
+        destination:'/destination',
+        permanent:false,
+      }
+    }
+    
+  }
+})
