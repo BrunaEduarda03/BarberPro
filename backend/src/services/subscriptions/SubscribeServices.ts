db321ab43d013a174c5877b2f4f37748ab0d4fa0
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-03-19 14:23:41 -0300

    finalizado rota de pagamentos

--- backend/src/services/subscriptions/SubscribeServices.ts
+++ backend/src/services/subscriptions/SubscribeServices.ts
@@ -1,3 +1,5 @@
+import Stripe from "stripe";
+import prismaClient from "../../prisma";
 
 interface SubscribeRequest{
   user_id:string;
@@ -5,7 +7,51 @@ interface SubscribeRequest{
 
 class SubscribeService{
   async execute({user_id}:SubscribeRequest){
-    return user_id;
-  }
+    const stripe = new Stripe(
+      process.env.STRIPE_API_KEY,{
+        apiVersion:"2022-11-15",
+        appInfo:{
+          name:'barberPro',
+          version:'1'
+        }
+      })
+    const findUser = await prismaClient.user.findFirst({
+      where:{
+        id: user_id,
+      }
+    })
+    let customerId = findUser.stripe_customer_id;
+
+    if(!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email:findUser.email
+      })
+      await prismaClient.user.update({
+        where:{
+          id:user_id,
+        },
+        data:{
+          stripe_customer_id:stripeCustomer.id
+        }
+      })
+      customerId = stripeCustomer.id;
+    }
+    //checkout stripe
+    const stripeCheckoutStripe = await stripe.checkout.sessions.create({
+      customer:customerId,
+      payment_method_types:['card'],
+      billing_address_collection:'required',
+      line_items:[
+        { price:process.env.STRIPE_PRICE,quantity:1 }
+      ],
+      mode:'subscription',
+      allow_promotion_codes:true,
+      success_url:process.env.STRIPE_SUCESS,
+      cancel_url:process.env.STRIPE_CANCEL_URL,
+    })
+    return {
+      sessionId:stripeCheckoutStripe.id
+    }
+  } 
 }
 export {SubscribeService}
